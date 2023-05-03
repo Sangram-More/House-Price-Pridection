e716ba5f3c5ac6c25169c546253e1d7b328041b2
Sangram <sangrammore2014@gmail.com>
2023-05-04 02:40:18 +0530

    Built front end website for the project

--- Codes/backend.py
+++ Codes/backend.py
@@ -1,4 +1,4 @@
-def ChooseModel(model_name, area, no_of_bedrooms, city, location, resale):
+def ChooseModel(area, no_of_bedrooms, city, location, resale, model_name = 'XGBoost'):
     
     if model_name == 'Linear Regression':
         LinearRegressionPrediction(area, no_of_bedrooms, city, location, resale)
@@ -27,12 +27,7 @@ def LinearRegressionPrediction(area, no_of_bedrooms, city, location, resale):
     input = [[area, no_of_bedrooms, city, location, resale]]
     y_predict_LR_Single = LR.predict(input)
     print(y_predict_LR_Single)
-    # print(y_predict_LR)
-    # LR_score = LR.score(X_test, y_test)
-    # print(LR_score*100)
-    # Cross Validation
-    # CVS_LR = cross_val_score(LR, data, price, cv=10).mean()
-    # CVP_LR= cross_val_predict(LR, X_test, y_test,  cv=10)
+
 
 #------------------------------------------------------------------------------------------------- 
 
@@ -46,11 +41,7 @@ def RidgeRegressionPrediction(area, no_of_bedrooms, city, location, resale):
     input = [[area, no_of_bedrooms, city, location, resale]]
     y_predict_RR_Single = RR.predict(input)
     print(y_predict_RR_Single)
-    # print(y_predict_RR)
-    # RR_score = RR.score(X_test, y_test)
-    # print(RR_score*100)
-    # CVS_RR = cross_val_score(RR, data, price, cv=10).mean()
-    # CVP_RR = cross_val_predict(RR, X_test, y_test,  cv=10)
+
 
 #-------------------------------------------------------------------------------------------------
 
@@ -63,10 +54,7 @@ def DecisionTreePrediction(area, no_of_bedrooms, city, location, resale):
     input = [[area, no_of_bedrooms, city, location, resale]]
     y_predict_DT_Single = DT.predict(input)
     print(y_predict_DT_Single)
-    # DT_score = DT.score(X_test, y_test)
-    # Cross Validation
-    # CVS_DT = cross_val_score(DT, data, price, cv=10).mean()
-    # CVP_DT= cross_val_predict(DT, X_test, y_test,  cv=10)
+
 
 #-------------------------------------------------------------------------------------------------
 
@@ -80,9 +68,7 @@ def RandomForestPrediction(area, no_of_bedrooms, city, location, resale):
     input = [[area, no_of_bedrooms, city, location, resale]]
     y_predict_RF_Single = RF.predict(input)
     print(y_predict_RF_Single)
-    # Cross Validation
-    # CVS_RF = cross_val_score(RF, data, price, cv=10).mean()
-    # CVP_RF = cross_val_predict(RF, X_test, y_test,  cv=10)
+
 #-------------------------------------------------------------------------------------------------
 
 def XGBoostPrediction(area, no_of_bedrooms, city, location, resale):
@@ -102,10 +88,7 @@ def XGBoostPrediction(area, no_of_bedrooms, city, location, resale):
     frame = df = pd.DataFrame(input)
     y_predict_XGB_single = XGB.predict(frame)
     print(y_predict_XGB_single)
-    # XGB_score = XGB.score(X_test, y_test)
-    # Cross Validation
-    # CVS_XGB = cross_val_score(XGB, data, price, cv=10).mean()
-    # CVP_XGB = cross_val_predict(XGB, X_test, y_test,  cv=10)
+
 
 # -----------------------------------------------------------------------------------------------------------
 
@@ -113,33 +96,41 @@ if __name__ == "__main__":
 
     import pandas as pd
     import numpy as np
-    import seaborn as sns
-    import matplotlib.pyplot as plt
     from sklearn.preprocessing import MinMaxScaler, LabelEncoder, OneHotEncoder
     from sklearn.model_selection import train_test_split,  cross_val_score, cross_val_predict
     from sklearn.metrics import r2_score, confusion_matrix
     from sklearn import metrics
 
     import sys
-    area = int(sys.argv[1])
-    no_of_bedrooms = int(sys.argv[2])
-    city = str(sys.argv[3])
-    location = str(sys.argv[4])
-    resale = int(sys.argv[5])
-    model_name = str(sys.argv[6])
+    # area = int(sys.argv[1])
+    # no_of_bedrooms = int(sys.argv[2])
+    # city = str(sys.argv[3])
+    # location = str(sys.argv[4])
+    # resale = int(sys.argv[5])
+    # model_name = str(sys.argv[6])
+
+    area = 1675
+    no_of_bedrooms = 2
+    city = 'Banglore'
+    location = 'Doddanekundi'
+    resale = 0
+    model_name = 'Random Forest'
+
+    # print(area, no_of_bedrooms, city, location, resale, model_name)
 
     df = pd.read_csv('../CSV/India.csv', index_col=False)
 
     # Label Encoding
     label_encoder = LabelEncoder()
-    # print(label_encoder)
+
     df_encoded = df.copy()
     df_encoded['Location'] = label_encoder.fit_transform(df['Location'])
     Location_name_mapping = dict(zip(label_encoder.classes_,    label_encoder.transform(label_encoder.classes_)))
-    # print(Location_name_mapping)
+
     df_encoded['City'] = label_encoder.fit_transform(df['City'])
     city_name_mapping = dict(zip(label_encoder.classes_, label_encoder.transform(label_encoder.classes_)))
-    # print(city_name_mapping)
+
+
 
     df1 = df_encoded[['Price','Area', 'No. of Bedrooms', 'City', 'Location', 'Resale']]
     df1.head()
@@ -151,7 +142,6 @@ if __name__ == "__main__":
 
     # Train Test split
     X_train, X_test, y_train, y_test = train_test_split(data, price, test_size=0.2)
-    # print(len(X_train), len(X_test), len(y_train), len(y_test))
 
     #Parameters
     # area = 3340
@@ -160,7 +150,6 @@ if __name__ == "__main__":
     location = int(Location_name_mapping.get(location))
     # resale = 0
     # model_name = 'XGBoost'
-
 
     ChooseModel(model_name, area, no_of_bedrooms, city, location, resale)
 
--- Codes/price-predict.js
+++ Codes/price-predict.js
@@ -2,7 +2,7 @@ const { spawn } = require('child_process');
 
 //const childPython = spawn('python', ['--version']);
 // const childPython2 = spawn('python', ['backend.py', 'Area', 'No. of Bedrooms', 'City', 'Location', 'Resale']);
-const childPython = spawn('python', ['backend.py', '3340', '4', 'Banglore', 'JP Nagar Phase 1', '0', 'XGBoost']);
+const childPython = spawn('python', ['backend.py', '1675', '3', 'Banglore', 'Doddanekundi', '0', 'Random Forest']);
 
 childPython.stdout.on('data', (data) => {
     console.log(`stdout: ${data}`);
