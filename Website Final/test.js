593e34e76f818f8da75bb8c8b0fb8ad6b24b58be
Sangram <sangrammore2014@gmail.com>
2023-05-24 13:10:09 +0530

    Final commit

--- Website Final/test.js
+++
@@ -1,6 +0,0 @@
-function display(){
-    let nob = parseInt(document.getElementById('nob').value);
-    let models = document.getElementById('models').value;
-    document.writeln(nob, " ", typeof nob);
-    document.writeln('<br>', models, " ", typeof models);
-}
